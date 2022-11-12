0bd2a205faec2e454b6823bf3fbe4a8cf6c72af3
Mike Whaley <michael.d.whaley@gmail.com>
2022-11-12 16:45:21 -0500

    Fully Functional

--- calc.js
+++ calc.js
@@ -26,6 +26,17 @@ reader.question("What would you like to calculate?", function(input){
 		console.log(num1 - num2)
 	}
 
+	if (mathSymbol === "*") {
+		console.log(num1 * num2)
+	}
+
+	if (mathSymbol === "/") {
+		console.log(num1 / num2)
+	}
+
+	if (mathSymbol === "sqrt") {
+		console.log(Math.sqrt(num1))
+	}
 
 	// This line closes the connection to the command line interface.
 	reader.close()

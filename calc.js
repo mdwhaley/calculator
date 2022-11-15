5c8a9763e50f011c0f9c6357449c807234d4fea8
Mike Whaley <michael.d.whaley@gmail.com>
2022-11-14 20:53:56 -0500

    Removed remainder

--- calc.js
+++ calc.js
@@ -38,10 +38,6 @@ reader.question("What would you like to calculate?", function(input){
 		console.log(Math.sqrt(num1))
 	}
 
-	if (mathSymbol === "%") {
-		console.log(num1 % num2)
-	}
-
 	// This line closes the connection to the command line interface.
 	reader.close()
 
